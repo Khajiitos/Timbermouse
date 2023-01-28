f6ff3d9840ca2680eb4a84e2ec72a071591db429
Khajiitos <khajiit112@gmail.com>
2023-01-28 21:24:16 +0100

    Add close button to gameover

--- src/events/eventNewPlayer.lua
+++ src/events/eventNewPlayer.lua
@@ -1,7 +1,6 @@
 function eventNewPlayer(playerName)
-	initPlayer(playerName)
     tfm.exec.respawnPlayer(playerName)
-    tfm.exec.changePlayerSize(playerName, MOUSE_SIZE)
+	initPlayer(playerName)
     for player, _ in pairs(tfm.get.room.playerList) do
         if playerData[player].hidden then
             hideMouseFor(player, playerName)
